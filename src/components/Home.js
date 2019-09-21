ba2a3dd63521bf1cf08856248fb2895ca62f0006
Abraham Gnanasingh <abraham1991jpr@gmail.com>
2019-09-21 20:59:01 +0530

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  
+    render() {
+      return (
+        <h3 className="text-center mT5">An Apple</h3>
+      ); 
+    }
+}
+
+export default Home;
