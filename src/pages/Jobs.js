2bc30773e953c2db81f72c70815f43896c288c4e
Abraham Gnanasingh <abraham1991jpr@gmail.com>
2019-10-01 01:12:01 +0530

    Code changes

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal';
+import Pagination from 'react-bootstrap/Pagination';
 
 class Jobs extends Component {
     state = {
       showDeleteJobModal: false,
-      jobs : [
-        { id: '1001', name: 'Repair AC', value1: 'ipsum', value2: 'dolor' },
-        { id: '1002', name: 'RO Water Purifier Service', value1: 'consectetur', value2: 'adipiscing' },
-        { id: '1003', name: 'Repair Washing Machine', value1: 'nec', value2: 'odio' }
-      ],
-      selectedJob: null
+      jobs: {
+        meta: { count: 21 },
+        list: [
+          { id: '1001', name: 'Repair AC', createdOn: 'ipsum', address: '1234 Main St' },
+          { id: '1002', name: 'RO Water Purifier Service', createdOn: 'consectetur', address: '25 Church St' },
+          { id: '1003', name: 'Repair Washing Machine', createdOn: 'nec', address: '77 Eden St' }
+        ]
+      },
+      selectedJob: null,
+      activePage: 1,
+      itemsPerPage: 10,
+      totalPages: 1
+    }
+
+    componentDidMount() {
+      const { jobs, itemsPerPage } = this.state;
+
+      let totalPages = jobs ? Math.ceil(jobs.meta.count/itemsPerPage) : 0;
+      this.setState({totalPages});
+    }
+
+    handlePaginationChange(elem) {
+      let { activePage, totalPages } = this.state;
+
+      let currentPage = activePage;
+
+      switch(elem) {
+        case 'first': currentPage = 1; break;
+        case 'prev': currentPage = activePage - 1; break;
+        case 'next': currentPage = activePage + 1; break;
+        case 'last': currentPage = totalPages; break;
+        default: currentPage = elem; break;
+      }
+
+      this.setState({activePage: currentPage});
     }
 
     handleDeleteJobModalClose() {
@@ -30,7 +60,13 @@ class Jobs extends Component {
     }
   
     render() {
-      const { jobs, showDeleteJobModal, selectedJob } = this.state;
+      const { jobs, showDeleteJobModal, selectedJob, activePage, itemsPerPage, totalPages } = this.state;
+
+      let paginationItems = [];
+
+      for(let i = 1; i <= totalPages; i++) {
+        paginationItems.push(<Pagination.Item active={i === activePage} onClick={() => this.handlePaginationChange(i)}>{i}</Pagination.Item>);
+      }
 
       return (
         <div className="col-md-12 mt-3">
@@ -48,25 +84,44 @@ class Jobs extends Component {
             </div>
           </div>
 
-          <div className="table-responsive">
+          {/* <div className="row mb-2">
+            <div className="col-md-5">
+              <div className="list-group">
+              {jobs && jobs.list.map((j, index) => {
+                return (
+                  <Link key={j.id} to={`/jobs/details/${j.id}`} className={`list-group-item list-group-item-action ${index === 0 ? 'active' : ''}`}>
+                    <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">{j.name}</h5>
+                      <small className={`${index !== 0 ? 'text-muted' : ''}`}>3 days ago</small>
+                    </div>
+                    <p className="mb-1">{j.address}</p>
+                    <small className={`${index !== 0 ? 'text-muted' : ''}`}>Donec id elit non mi porta.</small>
+                  </Link>
+                )
+              })}
+              </div>
+            </div>
+          </div> */}
+
+          <div className="table-responsive mb-2">
             <table className="table table-striped table-bordered">
               <thead>
                 <tr className="text-center">
                   <th>#</th>
                   <th>Name</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>Address</th>
+                  <th>Created On</th>
                   <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
-                {jobs.map(j => {
+                {jobs && jobs.list.map(j => {
                   return (
                     <tr className="text-center" key={j.id}>
                       <td className="align-middle">{j.id}</td>
                       <td className="align-middle">{j.name}</td>
-                      <td className="align-middle">{j.value1}</td>
-                      <td className="align-middle">{j.value2}</td>
+                      <td className="align-middle">{j.address}</td>
+                      <td className="align-middle">{j.createdOn}</td>
                       <td className="align-middle">
                         <Link to={`/jobs/edit/${j.id}`} className="btn btn-link py-0">Edit</Link>
                         <button className="btn btn-link py-0" onClick={() => this.handleDeleteJobConfirm(j)}>Delete</button>
@@ -78,14 +133,34 @@ class Jobs extends Component {
             </table>
           </div>
 
+          <div className="mb-2">
+            <div className="d-inline-block">
+              <p><span className="font-weight-bold">Total Count:</span> {jobs && (jobs.meta.count === 0 ? '###' : jobs.meta.count)}</p>
+            </div>
+            <div className="float-md-right">
+              <Pagination>
+                <Pagination.First disabled={activePage === 1} onClick={() => this.handlePaginationChange('first')} />
+                <Pagination.Prev disabled={activePage === 1} onClick={() => this.handlePaginationChange('prev')} />
+                {/* <Pagination.Item onClick={() => this.handlePaginationChange(1)}>{1}</Pagination.Item>
+                <Pagination.Ellipsis /> */}
+                
+                { React.Children.toArray(paginationItems) }
+
+                <Pagination.Next disabled={activePage === totalPages} onClick={() => this.handlePaginationChange('next')} />
+                <Pagination.Last disabled={activePage === totalPages} onClick={() => this.handlePaginationChange('last')} />
+              </Pagination>
+            </div>
+            <div className="clearfix"></div>
+          </div>
+
           <Modal show={showDeleteJobModal} onHide={() => this.handleDeleteJobModalClose()}>
             <Modal.Header closeButton>
               <Modal.Title>Delete Job</Modal.Title>
             </Modal.Header>
             <Modal.Body>Are you sure want to delete the job (<span dangerouslySetInnerHTML={{__html: `<b>${selectedJob && selectedJob.name}</b>`}}></span>)?</Modal.Body>
             <Modal.Footer>
-              <button type="button" class="btn btn-secondary" onClick={() => this.handleDeleteJobModalClose()}>Close</button>
-              <button type="button" class="btn btn-danger" onClick={() => this.handleDeleteJob()}>Delete</button>
+              <button type="button" className="btn btn-secondary" onClick={() => this.handleDeleteJobModalClose()}>Close</button>
+              <button type="button" className="btn btn-danger" onClick={() => this.handleDeleteJob()}>Delete</button>
             </Modal.Footer>
           </Modal>
         </div>
