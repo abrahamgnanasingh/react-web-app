53884916abcd422564d369ac5a1ad136bc014c58
Abraham Gnanasingh <abraham1991jpr@gmail.com>
2019-10-13 23:20:34 +0530

    Code changes

---
+++ src/i18n/en_US.js
@@ -0,0 +1,10 @@
+I18n.properties = {
+    "signin.msg.title": "Please sign in",
+    "form.email.address": "Email address",
+    "form.password": "Password",
+    "form.remember.me": "Remember me",
+    "btn.signin": "Sign in",
+    "footer.msg.copyright": "All rights reserved"
+
+    
+};
