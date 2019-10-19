a8f4791da787573e557a879fc18ec18bf33b0144
Abraham Gnanasingh <abraham1991jpr@gmail.com>
2019-10-19 12:10:13 +0530

    Code changes

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,22 +4,13 @@ import PropTypes from 'prop-types';
 function Checkbox(props) {
     let { label, name, checked, onChange, isPartialSelect } = props;
 
+    let isChecked = isPartialSelect || checked;
+
     return (
         <label className={"checkcontainer " + (label ? "mb-0" : "")}>
-            {isPartialSelect ?
-                <div onClick={e => onChange && onChange(e)}>
-                    <i className="fa fa-minus-square-o fs-18" aria-hidden="false"></i>
-                    {label ? <span className="ml-2">{label}</span> : ""}
-                </div>
-            :
-            <>
-                {/* <span>{label}</span> */}
-                <input type="checkbox" name={name} checked={checked} onChange={e => onChange && onChange(e)} />
-                {/* <span className="checkmark"></span> */}
-                <i className={"fa " + (checked ? "fa-check-square" : (isPartialSelect ? "fa-minus-square-o" : "fa-square-o")) + " checkmark fs-18"} aria-hidden="false"></i>
-                {label ? <span className="ml-2">{label}</span> : ""}
-            </>
-            }
+            <input type="checkbox" name={name} checked={isChecked} onChange={e => onChange && onChange(e)} />
+            <i className={"fa " + (checked ? "fa-check-square" : (isPartialSelect ? "fa-minus-square-o" : "fa-square-o")) + " checkmark fs-18"} aria-hidden="false"></i>
+            {label ? <span className="ml-2">{label}</span> : ""}
         </label>
     )
 }
